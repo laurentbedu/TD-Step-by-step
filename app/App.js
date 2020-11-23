1c3c4ee3578c8ce74381d4fd6dbcb429b82b266d
laurentbedu <58790546+laurentbedu@users.noreply.github.com>
2020-11-23 10:03:18 +0100

    Fin step 0

--- app/App.js
+++ app/App.js
@@ -2,12 +2,25 @@ class App {
 
     static start() {
         //onpopstate
+        window.onpopstate = () => {
+            App.browse();
+        }
         //correction burger
+        $('.nav-link').on('click', (evt) => {
+            let btn = $(evt.target).closest('.navbar').find('.navbar-toggler').not('.collapsed');
+            btn ? btn.click() : null;
+        })
         //chargement de la page
+        $(document).ready(() => {
+            App.browse();
+        });
     }
 
     static browse() {
         //récupérer le hash et l'afficher dans main
+        console.clear();
+        let hash = (window.location.hash || "#accueil").substring(1);
+        $('#main').hide().html(hash).fadeIn();
     }
 
 }
