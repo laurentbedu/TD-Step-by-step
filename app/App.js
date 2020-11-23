51c00c75ac5abecd1d706e0afde5bb9162ffba2d
laurentbedu <58790546+laurentbedu@users.noreply.github.com>
2020-11-23 09:51:19 +0100

    first commit

---
+++ app/App.js
@@ -0,0 +1,13 @@
+class App {
+
+    static start() {
+        //onpopstate
+        //correction burger
+        //chargement de la page
+    }
+
+    static browse() {
+        //récupérer le hash et l'afficher dans main
+    }
+
+}
